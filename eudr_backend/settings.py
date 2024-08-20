12d226ad193c72270b183520ef97e1cda077a5fa
julesntare <julesntare@gmail.com>
2024-08-20 10:32:33 +0200

    changed path for STATIC_ROOT

--- eudr_backend/settings.py
+++ eudr_backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/var/www/html/static/"
 STATIC_URL = "static/"
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
 
 # Quick-start development settings - unsuitable for production
