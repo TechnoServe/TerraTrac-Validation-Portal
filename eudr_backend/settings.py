674783bb01c41d9bf0e1852a0d3365d09365fed9
julesntare <julesntare@gmail.com>
2024-08-20 09:51:28 +0200

    removed STATIC_ROOT

--- eudr_backend/settings.py
+++ eudr_backend/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
